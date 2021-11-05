3ab774cffe0b508fee6ca851824b679622b27a8c
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-11-04 20:54:46 -0400

    cannot get tests to work and of course no one is responding to me in the help channel

--- api/cats/cats-model.test.js
+++ api/cats/cats-model.test.js
@@ -1,13 +1,14 @@
 const Cats = require("./cats-model");
 const db = require("../../data/dbConfig");
 
-const gizmo = {name:"Gizmo"}
-const lucy = {name:"Lucy"}
+// const gizmo = {name:"Gizmo"}
+// const lucy = {name:"Lucy"}
 
 beforeAll(async ()=>{
   await db.migrate.rollback()
   await db.migrate.latest()
 })
+
 beforeEach(async ()=>{
   await db("cats").truncate()
 })
@@ -15,6 +16,7 @@ beforeEach(async ()=>{
 afterAll(async ()=>{
   await db.destroy()
 })
+
 it("correct env", ()=>{
   expect(process.env.DB_ENV).toBe("testing")
 })
